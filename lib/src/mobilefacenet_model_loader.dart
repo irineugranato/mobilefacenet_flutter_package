be715d4d8dd9611726a6b653b346f77f385727bc
Irineu Granato <irineugranato@Irineus-MacBook-Pro.local>
2026-05-19 17:10:59 -0300

    Fix package asset loading for MobileFaceNet

--- lib/src/mobilefacenet_model_loader.dart
+++ lib/src/mobilefacenet_model_loader.dart
@@ -1,16 +1,25 @@
+
+import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 
 class MobileFaceNetModelLoader {
   MobileFaceNetModelLoader._();
 
   static const String modelAssetPath =
-      'assets/models/mobilefacenet.tflite';
+      'packages/mobilefacenet_flutter_package/assets/models/mobilefacenet.tflite';
 
   static Future<Interpreter> loadInterpreter({
     InterpreterOptions? options,
   }) async {
-    return Interpreter.fromAsset(
-      modelAssetPath,
+    final byteData = await rootBundle.load(modelAssetPath);
+
+    final buffer = byteData.buffer.asUint8List(
+      byteData.offsetInBytes,
+      byteData.lengthInBytes,
+    );
+
+    return Interpreter.fromBuffer(
+      buffer,
       options: options,
     );
   }
